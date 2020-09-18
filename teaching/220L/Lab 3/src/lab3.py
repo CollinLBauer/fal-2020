25ca3d342b45c2a5d53610a69b0b2fd63da7cba5
Collin <collinlbauer@gmail.com>
2020-09-18 13:50:29 -0400

    Lab 3

--- teaching/220L/Lab 3/src/lab3.py
+++ teaching/220L/Lab 3/src/lab3.py
@@ -10,13 +10,26 @@ def power():
     print("Calculates the power of some base raised to some exponent.")
     # your code here
 
+def flag():
+    # There are lots
+    printStr = "\n"
+    for i in range(0, 5):
+        printStr += "* " * 6 + "=" * 33 + "\n"
+    for i in range(5, 10):
+        printStr += "=" * 45 + "\n"
+    print(printStr)
+
+            
+
 
 def main():
-    calc_pay()
+    # calc_pay()
     # power()
     # approx_sqrt()
     # alternate()
 	# ...
+
+    flag()
 
 
 main()
